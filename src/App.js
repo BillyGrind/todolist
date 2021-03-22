b2da6237e36c37982d62163b402e751a875bfb2c
BillyGrind <denayeranthony1782@gmail.com>
2021-03-22 15:11:20 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css' 
+import ToDo from './Todo.js'
+function App() {
+  return (
+    <div className="container">
+      <ToDo />
+    </div>
+  );
+}
+
+export default App;
